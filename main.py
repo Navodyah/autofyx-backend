2b9667c793f08afbefc805ba80c2f30f431fde75
navodyahansini <navodyahansini2002@gmail.com>
2026-01-05 16:21:24 +0530

    main python

--- main.py
+++ main.py
@@ -12,6 +12,19 @@ from starlette.middleware.cors import CORSMiddleware
 from config.mongodb import connect_to_mongodb, close_mongodb_connection
 
 from routes.user_routes import router as user_router
+from routes.brand_routes import router as brand_router
+from routes.model_routes import router as model_router
+
+from routes.engine_routes import router as engine_router
+from routes.fuel_routes import router as fuel_router
+
+from routes.maintenance_routes import router as maintenance_router
+from routes.oil_routes import router as oil_router
+
+from routes.transmission_routes import router as transmission_router
+
+from routes.vehicle_routes import router as vehicle_router
+from routes.vehicle_class_routes import router as vehicle_class_router
 
 
 @asynccontextmanager
@@ -33,3 +46,34 @@ app.add_middleware(
 
 
 app.include_router(user_router)
+
+app.include_router(brand_router)
+
+
+
+app.include_router(model_router)
+
+
+
+app.include_router(engine_router)
+
+
+app.include_router(fuel_router)
+
+
+app.include_router(maintenance_router)
+
+
+app.include_router(oil_router)
+
+
+
+app.include_router(transmission_router)
+
+
+
+app.include_router(vehicle_router)
+
+
+
+app.include_router(vehicle_class_router)

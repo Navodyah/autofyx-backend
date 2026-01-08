04981c79f8f3ad2aba73d537d67fefbdd243f2f6
navodyahansini <navodyahansini2002@gmail.com>
2026-01-09 01:41:21 +0530

    fix error

--- schemas/brand_schema.py
+++ schemas/brand_schema.py
@@ -1,26 +1,18 @@
 from pydantic import BaseModel
 from typing import Optional
 
-
-class BrandBase(BaseModel):
-    brand_name: str
-    country: Optional[str] = None
-
-
 class BrandCreate(BaseModel):
-    brand_id: int
     brand_name: str
     country: Optional[str] = None
-
-
 
 class BrandUpdate(BaseModel):
     brand_name: Optional[str] = None
     country: Optional[str] = None
 
-
-class BrandResponse(BrandBase):
+class BrandResponse(BaseModel):
     brand_id: int
+    brand_name: str
+    country: Optional[str] = None
 
     class Config:
         from_attributes = True

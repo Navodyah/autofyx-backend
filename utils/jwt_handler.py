d5dcdb7ed50e0c4640bfa103f2dbbd58909203a8
navodyahansini <navodyahansini2002@gmail.com>
2026-02-18 13:24:50 +0530

    feature: jwt handler file

---
+++ utils/jwt_handler.py
@@ -0,0 +1,13 @@
+# utils/jwt_handler.py
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+from config.settings import JWT_SECRET, JWT_ALGORITHM, JWT_EXPIRE_MINUTES
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=JWT_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+def decode_access_token(token: str) -> dict:
+    return jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
